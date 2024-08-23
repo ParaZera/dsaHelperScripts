cf566c78ff61226996797384385c95299f0ea6a1
Tim Schrader <tim.schrader92@gmail.com>
2024-08-23 21:48:19 +0200

    [wip] basis

--- update_cs.py
+++ update_cs.py
@@ -45,6 +45,138 @@ def modify_cell_content(skill_values: dict[str, str], cell: Tag) -> str:
     return cell.string
 
 
+def generate_titel_row(soup):
+    th1 = soup.new_tag("th", attrs={"class": "titel", "colspan": "2"})
+    th1.append("Meta-Talente")
+
+    tr1 = soup.new_tag("tr")
+    tr1.append(th1)
+
+    return tr1
+
+
+def generate_left_row(soup):
+    th_name = soup.new_tag("th", attrs={"class": "name", "colspan": "2"})
+    th_name.append("Meta-Talente")
+
+    th_taw = soup.new_tag("th", attrs={"class": "taw"})
+    th_taw.append("TAW")
+
+    tr_head = soup.new_tag("tr")
+    tr_head.append(th_name)
+    tr_head.append(th_taw)
+
+    td_name = soup.new_tag("td", attrs={"class": "name"})
+    td_name.append("I AM NAME")
+
+    td_probe = soup.new_tag("td", attrs={"class": "probe"})
+    td_probe.append("I AM PROBE")
+
+    td_taw = soup.new_tag("td", attrs={"class": "taw"})
+    td_taw.append("TAW")
+
+    tr1 = soup.new_tag("tr")
+    tr1.append(td_name)
+    tr1.append(td_probe)
+    tr1.append(td_taw)
+
+    td_name2 = soup.new_tag("td", attrs={"class": "name"})
+    td_name2.append("I AM NAME 2")
+
+    td_probe2 = soup.new_tag("td", attrs={"class": "probe"})
+    td_probe2.append("I AM PROBE 2")
+
+    td_taw2 = soup.new_tag("td", attrs={"class": "taw"})
+    td_taw2.append("99")
+
+    tr2 = soup.new_tag("tr")
+    tr2.append(td_name2)
+    tr2.append(td_probe2)
+    tr2.append(td_taw2)
+
+    tbody = soup.new_tag("tbody")
+    tbody.append(tr_head)
+    tbody.append(tr1)
+    tbody.append(tr2)
+
+    table = soup.new_tag("table", attrs={"class": "talentgruppe gitternetz"})
+    table.append(tbody)
+
+    div = soup.new_tag("div", attrs={"class": "links_innen"})
+    div.append(table)
+
+    td_links = soup.new_tag("td", attrs={"class": "links"})
+    td_links.append(div)
+
+    return td_links
+
+    # tr_upper = soup.new_tag("tr")
+    # tr_upper.append(td_links)
+
+    # return tr_upper
+
+
+def generate_right_row(soup):
+    th_name = soup.new_tag("th", attrs={"class": "name", "colspan": "2"})
+    th_name.append("Meta-Talente")
+
+    th_taw = soup.new_tag("th", attrs={"class": "taw"})
+    th_taw.append("TAW")
+
+    tr_head = soup.new_tag("tr")
+    tr_head.append(th_name)
+    tr_head.append(th_taw)
+
+    td_name = soup.new_tag("td", attrs={"class": "name"})
+    td_name.append("I AM NAME")
+
+    td_probe = soup.new_tag("td", attrs={"class": "probe"})
+    td_probe.append("I AM PROBE")
+
+    td_taw = soup.new_tag("td", attrs={"class": "taw"})
+    td_taw.append("TAW")
+
+    tr1 = soup.new_tag("tr")
+    tr1.append(td_name)
+    tr1.append(td_probe)
+    tr1.append(td_taw)
+
+    td_name2 = soup.new_tag("td", attrs={"class": "name"})
+    td_name2.append("I AM NAME 2")
+
+    td_probe2 = soup.new_tag("td", attrs={"class": "probe"})
+    td_probe2.append("I AM PROBE 2")
+
+    td_taw2 = soup.new_tag("td", attrs={"class": "taw"})
+    td_taw2.append("99")
+
+    tr2 = soup.new_tag("tr")
+    tr2.append(td_name2)
+    tr2.append(td_probe2)
+    tr2.append(td_taw2)
+
+    tbody = soup.new_tag("tbody")
+    tbody.append(tr_head)
+    tbody.append(tr1)
+    tbody.append(tr2)
+
+    table = soup.new_tag("table", attrs={"class": "talentgruppe gitternetz"})
+    table.append(tbody)
+
+    div = soup.new_tag("div", attrs={"class": "rechts_innen"})
+    div.append(table)
+
+    td_links = soup.new_tag("td", attrs={"class": "rechts"})
+    td_links.append(div)
+
+    return td_links
+
+    # tr_upper = soup.new_tag("tr")
+    # tr_upper.append(td_links)
+
+    # return tr_upper
+
+
 def apply_modification(skill_values: dict[str, str], soup):
     tables: ResultSet[Tag] = soup.find_all("table", class_="talentgruppe gitternetz")
     for table in tables:
@@ -57,38 +189,25 @@ def apply_modification(skill_values: dict[str, str], soup):
                     col.string = modified_cell
 
     table = soup.find("table", class_="talente")
-    meta_talents_table = soup.new_tag("table", class_="talente")
+    meta_talents_table = soup.new_tag("table", attrs={"class": "talente"})
     table.insert_after(meta_talents_table)
 
-    th1 = soup.new_tag("th", class_="titel", colspan="2")
-    th1.append("Meta-Talente")
-
-    tr1 = soup.new_tag("tr")
-    tr1.append(th1)
-
-    div_links_innen = soup.new_tag("div", class_="links_innen")
-    td_links = soup.new_tag("td", class_="links")
-    td_links.append(div_links_innen)
-
-    div_rechts_innen = soup.new_tag("div", class_="rechts_innen")
-    td_rechts = soup.new_tag("td", class_="rechts")
-    td_rechts.append(div_rechts_innen)
-
-    tr2 = soup.new_tag("tr")
-    tr2.append(td_links)
-    tr2.append(td_rechts)
-
     tbody = soup.new_tag("tbody")
-    tbody.append(tr1)
 
     meta_talents_table.append(tbody)
 
-    # tbody = soup.new_tag("tbody")
-    # tr1 = tbody.append("tr")
-    # tr1.append("td").append("th", class_="titel", colspan="2").append("Meta-Talente")
+    tr1_titel = generate_titel_row(soup)
+    tbody.append(tr1_titel)
 
-    # tr1 = tbody.append("tr")
-    # tr1.append("td").append("th", class_="titel", colspan="2").append("Meta-Talente")
+    tr2_body = soup.new_tag("tr")
+    left = generate_left_row(soup)
+    right = generate_right_row(soup)
+
+    tr2_body.append(left)
+    tr2_body.append(right)
+    tbody.append(tr2_body)
+
+    meta_talents_table.append(tbody)
 
 
 def rename_input_file(input_file: str):
